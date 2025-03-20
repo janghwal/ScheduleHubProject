49990d8e95b05f90a882bab5543b815ca1bd0796
janghwal <jhp337037@gmail.com>
2025-03-20 20:10:33 +0900

    chore: 초기 Spring 프로젝트 설정
    프로젝트 기본 구조 생성

---
+++ src/test/java/com/example/schedulehubproject/ScheduleHubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulehubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleHubProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
